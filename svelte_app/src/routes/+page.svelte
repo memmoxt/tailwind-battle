f9cce1c73df3bfa4c4b17d60854503cadf3ba948
memmoxt <willianfujii@gmail.com>
2024-06-30 23:45:44 -0300

    tailwind 1

--- svelte_app/src/routes/+page.svelte
+++ svelte_app/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import cssbattle from '$lib/images/today.png';
+    import Tailwind_Battle from './tailwind.svelte';
+</script>
+
+<div class="flex flex-col items-center justify-between">
+	<div style="height:300px;width:400px;" class="m-4 box-border">
+		<img
+			src={cssbattle}
+			alt="cssbattle"
+			class="cssbattle-image absolute"
+			style="opacity:0.5;height:300px;width:400px; z-index:-10;"
+		/>
+		<div class="height[300px] width[400px] absolute" style="z-index:-9;">
+			<!-- NOTE: Tailwind Battle -->
+            <Tailwind_Battle/>
+		</div>
+	</div>
+	<img src={cssbattle} alt="tailwindbattle" class="m-4" style="height:300px;width:400px;" />
+</div>
---
+++ svelte_app/src/routes/tailwind.svelte
@@ -0,0 +1,8 @@
+<div
+	class="flex h-[300px] w-[400px] flex-col items-center justify-start gap-y-[16.9q] bg-white px-[17q] pt-[16.5q]"
+>
+	<div class="h-[59.5q] w-full rounded-2xl bg-blue-500"></div>
+	<div class="h-[59.50q] w-full rounded-2xl bg-orange-400"></div>
+	<div class="h-[59.0q] w-full rounded-2xl bg-red-400"></div>
+</div>
+
